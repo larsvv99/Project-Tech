822f202fe0572fc38b541fc9e8b7f9026f399c6c
larsvv99 <lars.van.veldhuizen@hva.nl>
2023-03-14 11:03:40 +0100

    API client side aanmaken

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,12 @@
 /* eslint-disable indent */
+// eslint-disable-next-line no-undef
 const h4 = document.querySelector('h4');
 
 fetch('https://www.themealdb.com/api/json/v1/1/random.php')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-        h4.innerHTML = data.idMeal;
+        console.log(data.meals[0]);
+        h4.innerHTML = data.meals[0].strMeal;
     });
 
-console.log('test');
+
